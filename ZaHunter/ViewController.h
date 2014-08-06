0e83ec08f0840f7999b0f6fcd0acaf81af0174aa
Efrén Reyes <efren.reyes@globant.com>
2014-08-06 14:18:15 -0500

    Initial Commit

---
+++ ZaHunter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ZaHunter
+//
+//  Created by Efrén Reyes Torres on 8/6/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ZaHunter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZaHunter
+//
+//  Created by Efrén Reyes Torres on 8/6/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
