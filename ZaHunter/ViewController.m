3d7c9d1f9f6589519872e7da2b3f6b2dc1bc3956
Efrén Reyes <efren.reyes@globant.com>
2014-08-07 10:34:49 -0500

    Almost finished step 5, it needs to display the total travel time

--- ZaHunter/ViewController.m
+++ ZaHunter/ViewController.m
@@ -16,6 +16,9 @@
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
 @property NSArray *mapItems;
 @property CLLocation *userLocation;
+@property UILabel *tableFooter;
+@property NSMutableArray *travelTimes;
+@property NSMutableArray *pizzariaLocations;
 @end
 
 @implementation ViewController
@@ -23,9 +26,20 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.travelTimes = [[NSMutableArray alloc] init];
+    self.pizzariaLocations = [[NSMutableArray alloc] init];
     self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.delegate = self;
     [self.locationManager startUpdatingLocation];
+    CGRect footerRect = CGRectMake(0, 0, 320, 40);
+    self.tableFooter = [[UILabel alloc] initWithFrame:footerRect];
+    self.tableFooter.textColor = [UIColor blueColor];
+    self.tableFooter.backgroundColor = [self.tableView backgroundColor];
+    self.tableFooter.opaque = YES;
+    self.tableFooter.font = [UIFont boldSystemFontOfSize:15];
+    self.tableFooter.numberOfLines = 3;
+    self.tableFooter.text = @"Test";
+    self.tableView.tableFooterView = self.tableFooter;
 }
 
 
@@ -55,27 +69,64 @@
 
 			return [distanceObj1 compare:distanceObj2];
 		}];
+
+        if (mapItems.count > 4) {
+            for (int i = 0; i < 4; i++) {
+                [self.pizzariaLocations addObject:[mapItems objectAtIndex:i]];
+            }
+        }
         
         self.mapItems = mapItems;
+
+        MKMapItem *currentLocation = [MKMapItem mapItemForCurrentLocation];
+
+        for (MKMapItem *nextLocation in self.pizzariaLocations) {
+            [self getDirectionsFromPoint:currentLocation toPoint:nextLocation];
+            currentLocation = nextLocation;
+        }
+
+        double totalTravelTime = 0;
+        int counter = 1;
+        for (NSDecimalNumber *time in self.travelTimes) {
+            totalTravelTime += ([time doubleValue] + 50);
+
+        }
+
+        int hours = floor(totalTravelTime/(60*60));
+        int minutes = floor((totalTravelTime/60) - hours * 60);
+        int seconds = floor(totalTravelTime - (minutes * 60) - (hours * 60 * 60));
+
+        self.tableFooter.text = [NSString stringWithFormat: @"It will take you about %d:%d:%d hrs\nto hunt all the za...", hours, minutes, seconds];
         [self.tableView reloadData];
+    }];
+}
+
+-(void)getDirectionsFromPoint:(MKMapItem *)mapItemA toPoint:(MKMapItem *)mapItemB {
+    MKDirectionsRequest *request = [MKDirectionsRequest new];
+    request.source = mapItemA;
+    request.destination = mapItemB;
+    request.transportType = MKDirectionsTransportTypeWalking;
+    request.requestsAlternateRoutes = NO;
+
+    MKDirections *directions = [[MKDirections alloc] initWithRequest:request];
+    [directions calculateDirectionsWithCompletionHandler:^(MKDirectionsResponse *response, NSError *error) {
+        MKRoute *route = response.routes.firstObject;
+        [self.travelTimes addObject:[NSDecimalNumber numberWithFloat:route.expectedTravelTime]];
+        NSLog(@"%f", route.expectedTravelTime);
     }];
 }
 
 #pragma mark TableViewDelegate
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"pizzariaCellID"];
-    MKMapItem *item = [self.mapItems objectAtIndex:indexPath.row];
+    MKMapItem *item = [self.pizzariaLocations objectAtIndex:indexPath.row];
     cell.textLabel.text = item.name;
     cell.detailTextLabel.text = [NSString stringWithFormat:@"%f km", [self.userLocation distanceFromLocation:[item placemark].location] / 1000.0f];
     return cell;
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    if (self.mapItems.count > 4) {
-        return 4;
-    } else {
-        return self.mapItems.count;
-    }
+        return self.pizzariaLocations.count;
 }
 
 #pragma mark CLLocationManagerDelegate
